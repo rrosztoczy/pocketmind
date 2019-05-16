fe648b1d1d446e50d5307b9f7c13fc283c2d90e4
rrosztoczy <rrosztoczy@gmail.com>
2019-05-16 13:40:57 -0400

    created memory card

--- src/Components/NewMemoryForm.js
+++ src/Components/NewMemoryForm.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { Form } from 'semantic-ui-react'
+import { Card, Button } from 'semantic-ui-react'
 
   export default class NewMemoryForm extends React.Component {
 
@@ -18,24 +18,41 @@ import { Form } from 'semantic-ui-react'
     // }
     state = {}
 
-    handleChange = (event) => this.setState({[event.target.name]: event.target.value}, () => console.log("Form State", this.state))
+    // handleChange = (event) => this.setState({[event.target.name]: event.target.value}, () => console.log("Form State", this.state))
 
     render() {
       return (
-        <Form onSubmit={(event, newMemory) => this.props.handleSubmitNew(event, this.state)}>
-          <Form.Group widths='equal'>
-            <Form.Input onChange={this.handleChange} fluid label='Summary' placeholder='Summary' name="summary" value={this.state.summary ? this.state.summary : ""} />
-            {/* Figure out selection value */}
-            <Form.Input onChange={this.handleSelect} fluid label='Type' placeholder='Type' name="trumpet_type" value={this.state.trumpet_type ? this.state.trumpet_type : ""} />
-          </Form.Group>
-          <Form.Group widths='equal'>
-          <Form.Input onChange={this.handleChange} fluid label='Website URL' placeholder='Website URL' name="url" value={this.state.url ? this.state.url : ""} />
-            <Form.Input onChange={this.handleChange} fluid label='Root Url' placeholder='Root Url' name="root_url" value={this.state.root_url ? this.state.root_url : ""} />
-          </Form.Group>
-          <Form.TextArea onChange={this.handleChange} label='Content' placeholder='Tell us more about you...' name="content" value={this.state.content} />
-          {/* <Form.Checkbox label='I agree to the Terms and Conditions' /> */}
-          <Form.Button >Create</Form.Button>
-        </Form>
+        <Card centered>
+        <Card.Content>
+          <Card.Header>Create a Memory</Card.Header>
+          <Card.Meta>
+            What do you want to remember?
+          </Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='teal'>
+              Emotion
+            </Button>
+            <Button.Or />
+            <Button basic color='blue'>
+              Thought
+            </Button>
+          </div>
+          <Card.Description>
+            Are you feeling any
+          </Card.Description>
+          <div className='ui two buttons'>
+            <Button basic color='teal'>
+              Stress
+            </Button>
+            <Button.Or />
+            <Button basic color='blue'>
+              Anxiety
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
       )
     }
 }
