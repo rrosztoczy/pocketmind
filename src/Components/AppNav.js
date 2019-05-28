7da1b5744b72c58a34b00ce1b7ec492b33d1ce6b
rrosztoczy <rrosztoczy@gmail.com>
2019-05-28 16:15:53 -0400

    fixing thought and activity option formatting and consolidating component files

--- src/Components/AppNav.js
+++ src/Components/AppNav.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import { Menu, Image } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom'
+import { updateThoughtSelection, updateActivitySelection } from '../actions'
 
 export default class AppNav extends Component {
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  handleThoughtMenuClick = (e, { name }) => {
+    updateThoughtSelection({target: {value: 'journal'}})
+    this.handleItemClick(e, { name })
+  }
+
+  handleActivityMenuClick = (e, { name }) => {
+    updateActivitySelection({target: {value: 'work'}})
+    this.handleItemClick(e, { name })
+  }
 
   state = { activeItem: 'login' }
 
@@ -17,8 +28,8 @@ export default class AppNav extends Component {
         <Image src={require('../pmlogo.jpeg')} as='a' href='/landing-page' style={{width: '50px', height: 'auto', margin: '10px'}} floated="left" circular/>
           <Menu.Item as={NavLink} name='HOME' to='/home' active={activeItem === 'HOME'} onClick={this.handleItemClick} />
           <Menu.Item as={NavLink} name='MEMORIES' to='/profile' active={activeItem === 'MEMORIES'} onClick={this.handleItemClick} />
-          <Menu.Item as={NavLink} name='THOUGHTS' to='/thoughts' active={activeItem === 'THOUGHTS'} onClick={this.handleItemClick} />
-          <Menu.Item as={NavLink} name='ACTIVITIES' to='/activities' active={activeItem === 'ACTIVITIES'} onClick={this.handleItemClick} />
+          <Menu.Item as={NavLink} name='THOUGHTS' to='/thoughts' active={activeItem === 'THOUGHTS'} onClick={this.handleThoughtMenuClick} />
+          <Menu.Item as={NavLink} name='ACTIVITIES' to='/activities' active={activeItem === 'ACTIVITIES'} onClick={this.handleActivityMenuClick} />
           <Menu.Item as={NavLink} name='EMOTIONS' to='/emotions' active={activeItem === 'EMOTIONS'} onClick={this.handleItemClick} />
           <Menu.Item as={NavLink} name='TIMELINE' to='/timeline' active={activeItem === 'TIMELINE'} onClick={this.handleItemClick} />
           {/* <Menu.Item as={NavLink} name='LEFT BRAIN' to='/left-brain' active={activeItem === 'LEFT BRAIN'} onClick={this.handleItemClick} />
